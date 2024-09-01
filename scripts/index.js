5eb52086109efab72c5c86e2e448c1c81590d95b
Ksenia Parinova <mira.ken2016@gmail.com>
2024-09-01 15:06:54 +0300

    updait

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content; 
+const cardList = document.querySelector('.places__list'); 
+
+function createCard(item, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+    const deleteButton = cardElement.querySelector('.card__delete-button'); 
+    const cardImg = cardElement.querySelector('.card__image'); 
+    cardImg.src = item.link; 
+    cardImg.alt = item.name; 
+    cardElement.querySelector('.card__title').textContent = item.name; 
+    deleteButton.addEventListener('click', () => {
+      deleteCard(deleteButton); 
+    });
+    return cardElement;
+  }
+  
+  function addCard(cardElement) {
+    cardList.append(cardElement);
+  }
+
+  function deleteCard(button) {
+    button.closest('.card').remove();
+  }
+  
+initialCards.forEach(function(element) {
+const cardElement = createCard(element, deleteCard); 
+addCard(cardElement); 
+});
