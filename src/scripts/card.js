fcba058d35951c942a1d651054ca0705c7a98603
Ksenia Parinova <mira.ken2016@gmail.com>
2024-09-13 18:04:18 +0300

    правки

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,27 +1,27 @@
-import { cardTemplate } from './index.js';
+import { cardTemplate } from './index.js'; //импортируем переменную с шаблоном карточки
 
 function createCard(item, deleteCard, likeCard, openFullImage) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
-  const delButton = cardElement.querySelector('.card__delete-button'); 
-  const cardImg = cardElement.querySelector('.card__image'); 
-  const LikeButton = cardElement.querySelector('.card__like-button'); 
-  cardImg.src = item.link; 
-  cardImg.alt = item.name; 
-  cardElement.querySelector('.card__title').textContent = item.name; 
-  delButton.addEventListener('click', function() {
-    deleteCard(delButton); 
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонируем элемент карточки из шаблона в переменную
+  const delButton = cardElement.querySelector('.card__delete-button'); //забираем кнопку удаления для карточки
+  const cardImg = cardElement.querySelector('.card__image'); //забираем картинку карточки
+  const LikeButton = cardElement.querySelector('.card__like-button'); //забираем кнопку лайка
+  cardImg.src = item.link; //добавляем ссылку в источник картинки для карточки
+  cardImg.alt = item.name; //добавляем alt для картинки
+  cardElement.querySelector('.card__title').textContent = item.name; //добавляем название карточки
+  delButton.addEventListener('click', () => {
+    deleteCard(delButton); //по клику выполняем функцию удаления с аргументом-кнопкой
   });
-  LikeButton.addEventListener('click', likeCard);
-  cardImg.addEventListener('click', openFullImage);  
-  return cardElement; 
+  LikeButton.addEventListener('click', likeCard); //по клику на кнопку лайка запускаем функцию обработки лайков
+  cardImg.addEventListener('click', openFullImage); //по клику на карточку запускаем функцию проверки, был ли клик по картинке
+  return cardElement; //возвращаем элемент карточки
 }
 
 function deleteCard(button) {
-  button.closest('.card').remove(); 
+  button.closest('.card').remove(); //удаляем ближайший к кнопке-аргументу элемент карточки
 }
 
 function likeCard(evt) {
-  evt.target.classList.toggle('card__like-button_is-active');
+  evt.target.classList.toggle('card__like-button_is-active'); //переключаем класс постановки лайка
 }
 
 export { createCard, deleteCard, likeCard }; 
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,31 +1,31 @@
 function openPopup(popup) {
-  popup.classList.add('popup_is-opened');  
-  document.addEventListener('keydown', closePopupEscape);
+  popup.classList.add('popup_is-opened');  //открываем попап
+  document.addEventListener('keydown', closePopupEscape); //если будет нажатие Esc, выполнить функцию закрытия попапа
 }
 
 function closePopup(popup) {
-  popup.classList.remove('popup_is-opened'); 
-  document.removeEventListener('keydown', closePopupEscape); 
+  popup.classList.remove('popup_is-opened'); //закрываем попап
+  document.removeEventListener('keydown', closePopupEscape); //снять слушатель кнопки Esc
 }
 
 function closePopupEscape(evt) {
   if (evt.key === 'Escape') {
-    const popup = document.querySelector('.popup_is-opened');
-    closePopup(popup); 
+    const popup = document.querySelector('.popup_is-opened'); //записываем в локальную переменную открытый попап
+    closePopup(popup); //при нажатии клавиши Esc выполняем функцию закрытия попапа
   }
 };
 
 function closePopupButton(evt) {
   if (evt.target === evt.target.closest('.popup__close')) {
-    const popup = document.querySelector('.popup_is-opened');
-    closePopup(popup); 
+    const popup = document.querySelector('.popup_is-opened'); //записываем в локальную переменную открытый попап
+    closePopup(popup); //при клике по крестику выполняем функцию закрытия попапа
   }
 };
 
 function closePopupOverlay(evt) {
-  const popup = document.querySelector('.popup_is-opened');
+  const popup = document.querySelector('.popup_is-opened'); //записываем в локальную переменную открытый попап
   if (popup === evt.target) {    
-    closePopup(popup); 
+    closePopup(popup); //при клике по оверлею выполняем функцию закрытия попапа
   }};
 
 export {
@@ -34,4 +34,4 @@ export {
   closePopupEscape,
   closePopupButton,
   closePopupOverlay
-}; 
+};
